7465cdddd624466a3e6ff6930df40e81fcb4241a
drnisar <drnisar@hotmail.com>
2025-06-16 20:03:33 +0500

    mongodb migration success

---
+++ app/dashboard/ot/_components/SurgicalProceduresForSingleReg.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SurgicalProceduresForSingleReg = () => {
+  return <div>SurgicalProceduresForSingleReg</div>;
+};
+
+export default SurgicalProceduresForSingleReg;
