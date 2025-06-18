96069cc328f0f47d89f61f118f5e76687a50513f
drnisar <drnisar@hotmail.com>
2025-06-18 21:43:25 +0500

    mongodb success

--- app/dashboard/ot/_components/SurgicalProceduresForSingleReg.tsx
+++ app/dashboard/ot/_components/SurgicalProceduresForSingleReg.tsx
@@ -10,9 +10,16 @@ const SurgicalProceduresForSingleReg = async ({
   const admissions = await prisma.admission_Discharge.findMany({
     where: { registration_id: registration_id },
   });
+  const admission_ids = admissions.map((admission) => admission.admission_id);
+  const procedures = admission_ids.map(async (admission_id) => {
+    const surgical_procedures = await prisma.oT.findMany({
+      where: { admission_id: admission_id },
+    });
+    console.log("Surgical Procedures", surgical_procedures);
+  });
   return (
     <Card>
-      <div>surgical procedures</div>
+      <div>{JSON.stringify(procedures)}</div>
     </Card>
   );
 };
