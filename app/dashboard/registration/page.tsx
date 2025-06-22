70769eaebcaf51d0d15de070317078d489f8e2b9
drnisar <drnisar@hotmail.com>
2025-06-22 19:00:42 +0500

    prisma singleton applied

--- app/dashboard/registration/page.tsx
+++ app/dashboard/registration/page.tsx
@@ -2,9 +2,10 @@ import React, { Suspense } from "react";
 import RegistrationsTable from "./_components/RegistrationsTable";
 import CreateRegistrationButton from "./_components/CreateRegistrationButton";
 import RegistrationPageSkeleton from "./_components/skeletons/RegistrationsPageSkeleton";
-import { PrismaClient } from "@/generated/prisma";
+// import { PrismaClient } from "@/generated/prisma";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma"; // Adjust the import path as necessary
 
 const RegistrationPage = async () => {
   const registrations = await prisma.registration.findMany({
