56d453399034985af2e1753ac1b8c050335c02b5
drnisar <drnisar@hotmail.com>
2025-06-27 14:08:13 +0500

    minor correction

--- app/dashboard/registration/page.tsx
+++ app/dashboard/registration/page.tsx
@@ -2,7 +2,6 @@ import React, { Suspense } from "react";
 import RegistrationsTable from "./_components/RegistrationsTable";
 import CreateRegistrationButton from "./_components/CreateRegistrationButton";
 import RegistrationPageSkeleton from "./_components/skeletons/RegistrationsPageSkeleton";
-import { getRegistrations } from "@/app/actions/actions";
 
 const RegistrationPage = async () => {
   // const registrations = await getRegistrations();
