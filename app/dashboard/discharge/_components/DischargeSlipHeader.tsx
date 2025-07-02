a1486964aa82ebfe2d2f540e0ac991698534b5d1
drnisar <drnisar@hotmail.com>
2025-07-02 16:11:37 +0500

    discharge summary styling

--- app/dashboard/discharge/_components/DischargeSlipHeader.tsx
+++ app/dashboard/discharge/_components/DischargeSlipHeader.tsx
@@ -6,28 +6,38 @@ import React from "react";
 
 const DischargeHeader = () => {
   return (
-    <Flex justify="between">
-      <Box>
-        <Text as="p">Consultant General and Laparoscopic Surgeon</Text>
-        <Heading size="6" className="">
-          Dr Nisar Ahmed
-        </Heading>
-        <Text>
-          <em>MBBS, FCPS, MRCS, MHR</em>
-        </Text>
-        <Text as="p">Associate Professor of Surgery</Text>
-        <Text as="p">nisar.ahmed@kmc.edu.pk</Text>
-      </Box>
-      <Flex ml="5" justify="between">
-        <Image
-          src={KTH_logo}
-          width={75}
-          height={75}
-          alt={"kth logo"}
-          className="object-contain"
-        />
-        <Image src={kth_name} width={150} height={75} alt={"kth name"} />
+    <Flex direction="column">
+      <Flex justify="between">
+        <Box>
+          <Text as="p">Consultant General and Laparoscopic Surgeon</Text>
+          <Heading size="4" className="">
+            Dr Nisar Ahmed
+          </Heading>
+          <Text size="1">
+            <em>MBBS, FCPS, MRCS, MHR</em>
+          </Text>
+          <Text as="p" size="1">
+            Associate Professor of Surgery
+          </Text>
+          <Text as="p" size="1">
+            nisar.ahmed@kmc.edu.pk
+          </Text>
+        </Box>
+        <Flex ml="5" justify="between">
+          <Image
+            src={KTH_logo}
+            width={75}
+            height={75}
+            alt={"kth logo"}
+            className="object-contain"
+          />
+          <Image src={kth_name} width={150} height={75} alt={"kth name"} />
+        </Flex>
       </Flex>
+      <Heading size="6" className="uppercase" align="center">
+        Discharge Summary
+      </Heading>
+      <hr />
     </Flex>
   );
 };
