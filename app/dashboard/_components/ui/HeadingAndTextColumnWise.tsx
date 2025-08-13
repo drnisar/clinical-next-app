aed5cca7ee9f0cd21f154521ed8de8d97253ed89
drnisar <drnisar@hotmail.com>
2025-08-13 16:14:16 +0500

    bbit of change

--- app/dashboard/_components/ui/HeadingAndTextColumnWise.tsx
+++ app/dashboard/_components/ui/HeadingAndTextColumnWise.tsx
@@ -9,7 +9,7 @@ interface Props {
 const HeadingAndTextColumnWise = ({ heading, text, children }: Props) => {
   return (
     <div className="grid grid-cols-10 gap-4 mb-4 text-sm print:break-inside-avoid">
-      <div className="col-span-2 text-slate-700 ">{heading}</div>
+      <div className="col-span-2 light:text-slate-700 ">{heading}</div>
       <div className="col-span-8 whitespace-pre-line">
         {children ? children : text}
       </div>
